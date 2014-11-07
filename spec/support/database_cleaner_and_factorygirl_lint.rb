8c11818157b3162e3ee0ca6edd9a44dcd99f6277
Sam Lau <samlau95@gmail.com>
2014-11-07 01:26:36 -0800

    Initialize factory_girl

---
+++ spec/support/database_cleaner_and_factorygirl_lint.rb
@@ -0,0 +1,26 @@
+RSpec.configure do |config|
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.clean_with(:truncation)
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end

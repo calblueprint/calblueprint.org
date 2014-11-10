c8977b9885721aa177a31e1791f2e2f56eec2f87
Sam Lau <samlau95@gmail.com>
2014-11-09 21:04:24 -0800

    After sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(user)
+    if user.admin?
+      admin_dashboard_path
+    elsif user.member?
+      root_path
+    else
+      fail 'Bad user!'
+    end
+  end
+
   private
 
   def deny_access_if!(condition, message = nil)

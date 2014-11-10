82c7334f69e282287577c1361f352780d3ba7473
Sam Lau <samlau95@gmail.com>
2014-11-09 19:53:02 -0800

    Add dashboard and base controller

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,10 @@
+class BaseController < ApplicationController
+  before_action :authenticate_user!
+  before_filter :verify_logged_in_admin
+
+  private
+
+  def verify_logged_in_admin
+    deny_access_if! !current_user.admin?
+  end
+end
---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,2 @@
+class DashboardController < BaseController
+end

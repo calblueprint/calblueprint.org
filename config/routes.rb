da1e48072e6dd624d7dce273eb7a123477e7e6d5
Sam Lau <samlau95@gmail.com>
2014-11-09 19:52:50 -0800

    Add admin dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
+
+  devise_for :users
+
+  namespace :admin do
+    resource :dashboard, only: [:show], controller: 'dashboard'
+  end
 end

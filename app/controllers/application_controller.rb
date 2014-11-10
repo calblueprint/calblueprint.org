3f5ba063ab2df46c6474b8ca22c6caa89821b428
Sam Lau <samlau95@gmail.com>
2014-11-10 01:42:44 -0800

    Permit more params through devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Allow more params through devise
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_in_path_for(user)
     if user.admin?
       admin_dashboard_path
@@ -15,6 +18,11 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:accept_invitation) << [:first_name, :last_name]
+  end
+
+  # Helper for authorization
   def deny_access_if!(condition, message = nil)
     message ||= t('access_denied')
     redirect_to root_path, flash: { error: message } if condition

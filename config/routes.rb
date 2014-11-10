71aed284dc087fb94df2343204c081c6dcf68dd6
Sam Lau <samlau95@gmail.com>
2014-11-10 02:12:51 -0800

    Use new controller for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  devise_for :users
+  devise_for :users, controllers: { invitations: 'users/invitations' }
 
   namespace :admin do
     resource :dashboard, only: [:show], controller: 'dashboard'

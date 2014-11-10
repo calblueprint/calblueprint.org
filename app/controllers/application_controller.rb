fba0bd0a3707961f639b239d74fbc1f9435591fa
Sam Lau <samlau95@gmail.com>
2014-11-09 19:53:08 -0800

    Add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def deny_access_if!(condition, message = nil)
+    message ||= t('access_denied')
+    redirect_to root_path, error: message if condition
+  end
 end

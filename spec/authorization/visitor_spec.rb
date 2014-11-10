ffe95c132e59b224becaebb270f964b1957d3d13
Sam Lau <samlau95@gmail.com>
2014-11-09 23:49:29 -0800

    Finish visitor authorization spec

--- spec/authorization/visitor_spec.rb
+++ spec/authorization/visitor_spec.rb
@@ -1,12 +1,24 @@
 require "rails_helper"
 
 RSpec.describe "A visitor", type: :feature do
-  describe "can" do
-    subject { page }
+  subject { page }
 
-    describe "see the home page" do
+  describe "can see the" do
+    describe "home page" do
       before { visit root_path }
-      it { should have_content t('pages.home.title') }
+      it { should have_content t("pages.home.title") }
+    end
+
+    describe "log in page" do
+      before { visit new_user_session_path }
+      it { should have_content "Log in" }
+    end
+  end
+
+  describe "cannot see the" do
+    describe "admin dashboard" do
+      before { visit admin_dashboard_path }
+      it { should have_content t("devise.failure.unauthenticated") }
     end
   end
 end

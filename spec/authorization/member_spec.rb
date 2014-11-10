378e770efbdcb608251d36ce2a3c5aee4324546e
Sam Lau <samlau95@gmail.com>
2014-11-09 23:49:58 -0800

    Basic member spec

---
+++ spec/authorization/member_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "A member", type: :feature do
+  let(:member) { create :member }
+  before { sign_in member }
+
+  subject { page }
+
+  describe "cannot see the" do
+    describe "admin dashboard page" do
+      before { visit admin_dashboard_path }
+      it { should have_content t("access_denied") }
+    end
+  end
+end

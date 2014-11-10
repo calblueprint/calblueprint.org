caff88b1e11a0979092a1070288d4230e6b404d4
Sam Lau <samlau95@gmail.com>
2014-11-09 23:01:25 -0800

    User specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,5 +23,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should define_enum_for(:role).with([:member, :admin]) }
+
+  it { should validate_presence_of :first_name }
+  it { should validate_presence_of :last_name }
 end

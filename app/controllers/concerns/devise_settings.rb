9c656e11d790413a0e9de6ab1cb9f39ce30a2817
Sam Lau <samlau95@gmail.com>
2014-11-10 02:12:16 -0800

    Refactor devise settings into concern

---
+++ app/controllers/concerns/devise_settings.rb
@@ -0,0 +1,24 @@
+module DeviseSettings
+  extend ActiveSupport::Concern
+
+  included do
+    # Allow more params through devise
+    before_filter :configure_permitted_parameters, if: :devise_controller?
+  end
+
+  def after_sign_in_path_for(user)
+    if user.admin?
+      admin_dashboard_path
+    elsif user.member?
+      root_path
+    else
+      fail 'Bad user!'
+    end
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:accept_invitation) << [:first_name, :last_name]
+  end
+end

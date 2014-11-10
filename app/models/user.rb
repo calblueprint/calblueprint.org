5af4ac5c97284ade094a7fb6ce56585c40ea829c
Sam Lau <samlau95@gmail.com>
2014-11-09 19:48:12 -0800

    Change devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,10 +21,7 @@
 #
 
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 
   enum role: %w(member admin)
 end

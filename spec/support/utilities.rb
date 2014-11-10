df679e0b8fd382d6b7b13a7ed7d11185e38a1b30
Sam Lau <samlau95@gmail.com>
2014-11-09 23:49:53 -0800

    Spec utilities

---
+++ spec/support/utilities.rb
@@ -0,0 +1,8 @@
+# Methods that are used across specs
+
+def sign_in(user)
+  visit new_user_session_path
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_button "Log in"
+end

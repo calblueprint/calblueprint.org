5089a58560c053588ede0b036bddffc1661c5e21
Sam Lau <samlau95@gmail.com>
2014-11-11 12:12:23 -0800

    Fix paren error

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,7 +1,7 @@
 require_relative "production"
 
 Mail.register_interceptor(
-  RecipientInterceptor.new(ENV.fetch("EMAIL_RECIPIENTS", subject_prefix: '[STAGING]'))
+  RecipientInterceptor.new(ENV.fetch("EMAIL_RECIPIENTS"), subject_prefix: '[STAGING]')
 )
 
 Rails.application.configure do

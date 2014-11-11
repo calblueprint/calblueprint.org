ca2fb4550db4501ceb841c9b6bd0ce00c2bbaf53
Sam Lau <samlau95@gmail.com>
2014-11-11 01:29:47 -0800

    App generator config

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,16 @@ Bundler.require(*Rails.groups)
 
 module CalBlueprintOrgApp
   class Application < Rails::Application
+    # Generator config
+    config.generators do |generate|
+      generate.helper false
+      generate.javascript_engine false
+      generate.request_specs false
+      generate.routing_specs false
+      generate.stylesheets false
+      generate.test_framework :rspec
+      generate.view_specs false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

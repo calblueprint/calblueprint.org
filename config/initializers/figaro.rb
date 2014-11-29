f4c59c545f4040863ac44d2845005491a2c8be47
Sam Lau <samlau95@gmail.com>
2014-11-29 14:20:56 -0800

    Require email env variables in figaro

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,1 +1,2 @@
-Figaro.require_keys "SECRET_KEY_BASE", "DEVISE_SECRET_KEY"
+Figaro.require_keys "SECRET_KEY_BASE", "DEVISE_SECRET_KEY", "TEAM_EMAIL",
+                    "SMTP_ADDRESS", "SMTP_DOMAIN", "SMTP_PASSWORD", "SMTP_USERNAME"

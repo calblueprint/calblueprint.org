1150b752b77d52db1544885c6abdd8ab91791c19
Sam Lau <samlau95@gmail.com>
2014-11-29 15:30:27 -0800

    Spec for emailing

--- spec/features/contact_us_spec.rb
+++ spec/features/contact_us_spec.rb
@@ -7,10 +7,14 @@ RSpec.describe "Contact Us Form" do
   it { should have_content t("contact_forms.new.banner") }
 
   describe "without filling in form" do
-    before { click_button "Submit" }
     it "renders back form with errors" do
+      click_button "Submit"
       expect(page).to have_content t("contact_forms.new.banner")
       expect(page).to have_content "can't be blank"
+    end
+
+    it "does not send email" do
+      expect { click_button "Submit" }.not_to change { ActionMailer::Base.deliveries.count }
     end
   end
 
@@ -29,7 +33,11 @@ RSpec.describe "Contact Us Form" do
     end
 
     it "creates a ContactForm on submit" do
-      expect { click_button "Submit" }.to change(ContactForm, :count).by(1)
+      expect { click_button "Submit" }.to change { ContactForm.count }.by(1)
+    end
+
+    it "sends an email on submit" do
+      expect { click_button "Submit" }.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'shoulda/matchers'
+require 'sucker_punch/testing/inline'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are

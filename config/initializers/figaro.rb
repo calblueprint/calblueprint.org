317f074e0db5883708d13344f255af3c28c9c991
Sam Lau <samlau95@gmail.com>
2014-11-29 15:45:53 -0800

    Fix env variables for travis

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,2 +1,1 @@
-Figaro.require_keys "SECRET_KEY_BASE", "DEVISE_SECRET_KEY", "TEAM_EMAIL",
-                    "SMTP_ADDRESS", "SMTP_DOMAIN", "SMTP_PASSWORD", "SMTP_USERNAME"
+Figaro.require_keys "SECRET_KEY_BASE", "DEVISE_SECRET_KEY", "TEAM_EMAIL"

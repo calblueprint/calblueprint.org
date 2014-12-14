246e1d7163f376df15569f016c3e32be6d783c26
Sam Lau <samlau95@gmail.com>
2014-12-14 01:19:19 -0800

    Update seeds with applicant user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create! first_name: 'Sam', last_name: 'Just a Member', email: 'sam@member.com',
+User.create! first_name: 'Sam', last_name: 'Applicant', email: 'sam@applicant.com',
+             password: 'password', role: User.roles[:applicant]
+
+User.create! first_name: 'Sam', last_name: 'Member', email: 'sam@member.com',
              password: 'password', role: User.roles[:member]
 
-User.create! first_name: 'Sam', last_name: 'Lau', email: 'sam@admin.com',
+User.create! first_name: 'Sam', last_name: 'Admin', email: 'sam@admin.com',
              password: 'password', role: User.roles[:admin]

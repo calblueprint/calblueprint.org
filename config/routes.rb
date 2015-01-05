5d2c629778208bf52cc930b90e88c1b24d6c2431
Sam Lau <samlau95@gmail.com>
2015-01-04 17:14:31 -0800

    Make apply routes nice and use double quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
-  get '/about', to: 'pages#about'
-  get '/sponsors', to: 'pages#sponsors'
+  root to: "pages#home"
+  get "/about", to: "pages#about"
+  get "/sponsors", to: "pages#sponsors"
 
-  devise_for :users, controllers: { invitations: 'users/invitations' }
+  devise_for :users, controllers: { invitations: "users/invitations" }
 
   resources :contact_forms, only: [:new, :create]
 
   namespace :admin do
-    resource :dashboard, only: [:show], controller: 'dashboard'
+    resource :dashboard, only: [:show], controller: "dashboard"
   end
 
-  resources :apply, only: [:index]
-  scope '/apply' do
-    get 'students', to: 'apply#students'
-    get 'nonprofits', to: 'apply#nonprofits'
+  resource :apply, only: [:show], controller: "apply" do
+    get "students"
+    get "nonprofits"
   end
 end

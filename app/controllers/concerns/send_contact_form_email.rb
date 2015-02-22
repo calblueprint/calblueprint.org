7cebb7372b6912fea3461808a09303cf0333cb4d
Sam Lau <samlau95@gmail.com>
2015-02-21 18:20:02 -0800

    Rename workers to jobs

--- app/controllers/concerns/send_contact_form_email.rb
+++ app/controllers/concerns/send_contact_form_email.rb
@@ -1,5 +1,5 @@
 class SendContactFormEmail
   def self.execute(contact_form)
-    ContactFormMailerWorker.new.async.perform contact_form
+    ContactFormMailerJob.new.async.perform contact_form
   end
 end

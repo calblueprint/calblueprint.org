6ee664ab614b47b833cd5bbac2e6343681d8ffbe
cptincredble <cptincredble@gmail.com>
2015-03-07 19:43:31 -0800

    Add members route and view

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   namespace :admins, as: :admin do
     resource :dashboard, only: [:show], controller: "dashboard"
     resources :apps, only: [:index]
+    resources :members, only: [:index]
   end
 
   resource :apply, only: [:show], controller: "apply" do

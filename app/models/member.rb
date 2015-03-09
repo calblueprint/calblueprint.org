6d86e0cc8f84c56ded12165337328f7aa73bfe2e
cptincredble <cptincredble@gmail.com>
2015-03-08 21:51:26 -0700

    Add gravatar to member

--- app/models/member.rb
+++ app/models/member.rb
@@ -14,7 +14,4 @@ class Member < ActiveRecord::Base
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :email, presence: true
-
-  def self.retrieve_gravatar
-  end
 end

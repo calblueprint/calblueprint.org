625369cd42c887ec03f87c086ff678c2a0d604cf
cptincredble <cptincredble@gmail.com>
2015-03-08 19:19:36 -0700

    Add create new member

--- app/controllers/admins/members_controller.rb
+++ app/controllers/admins/members_controller.rb
@@ -4,9 +4,29 @@ module Admins
       @members = Member.all
     end
 
+    def new
+      @member = Member.new
+    end
+
+    def create
+      @member = Member.new(member_params)
+      if @member.save
+        redirect_to action: 'index'
+      else
+        # TODO: add banner describing why form was invalid
+        redirect_to action: 'new'
+      end
+    end
+
     def destroy
       Member.find_by(id: params[:id]).destroy
       redirect_to action: 'index'
+    end
+
+    private
+
+    def member_params
+      params.require(:member).permit(:last_name, :first_name, :email)
     end
   end
 end

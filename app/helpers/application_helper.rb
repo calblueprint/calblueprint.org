4b4852d0e73cf1491b2f86077bedddc335378fc2
cptincredble <cptincredble@gmail.com>
2015-03-09 19:06:23 -0700

    Fix guard complaint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def gravatar_url(email)
-    gravatar_hash = Digest::MD5::hexdigest(email).downcase
+    gravatar_hash = Digest::MD5.hexdigest(email).downcase
     "http://gravatar.com/avatar/#{gravatar_hash}.png"
   end
 end

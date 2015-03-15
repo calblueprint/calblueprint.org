c2a899fecf1b237e609140a05f9ff66373ae5fc9
cptincredble <cptincredble@gmail.com>
2015-03-15 14:46:23 -0700

    Add semester migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308033616) do
+ActiveRecord::Schema.define(version: 20150315214321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,13 @@ ActiveRecord::Schema.define(version: 20150308033616) do
     t.string   "link"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "semesters", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "season"
+    t.string   "year"
   end
 
 end

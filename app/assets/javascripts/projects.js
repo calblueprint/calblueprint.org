a5ae4fb7bad5ef82c787c565f21650fe3c1a54c6
wonjun <jeong.wonjun@gmail.com>
2015-04-04 18:54:37 -0700

    Add jquery animation for project card expand

---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+  $( ".project-see-more" ).click(function () {
+    var expand = $(this).parent().parent().children(".project-expand")
+    if (expand.is( ":hidden" ) ) {
+      expand.slideDown( "slow" );
+    } else {
+      expand.slideUp( "slow" );
+    }
+  });
+});
+

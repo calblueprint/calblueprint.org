6a28bd56b0cc48a344df2b9e282fb6bdee964ca5
cptincredble <cptincredble@gmail.com>
2015-04-13 19:49:50 -0700

    Annotate member & memberroles

--- spec/factories/member_roles.rb
+++ spec/factories/member_roles.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: member_roles
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime
+#  updated_at :datetime
+#  role       :string(255)
+#
+
 FactoryGirl.define do
   factory :member_role do
     role "Factory Role"
--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: members
+#
+#  id             :integer          not null, primary key
+#  created_at     :datetime
+#  updated_at     :datetime
+#  last_name      :string(255)
+#  first_name     :string(255)
+#  email          :string(255)
+#  member_role_id :integer
+#
+
 FactoryGirl.define do
   factory :member do
     sequence(:last_name) { |n| "#{n}" }

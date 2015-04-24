f879d8ca69c33ade6b695f0d54c0f8d76e9d959c
cptincredble <cptincredble@gmail.com>
2015-04-23 22:39:09 -0700

    Annotate settings

--- spec/models/settings_spec.rb
+++ spec/models/settings_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: settings
+#
+#  id                  :integer          not null, primary key
+#  created_at          :datetime
+#  updated_at          :datetime
+#  singleton_guard     :integer
+#  current_semester_id :integer
+#  npo_app_open        :boolean
+#  student_app_open    :boolean
+#
+
 require 'rails_helper'
 
 RSpec.describe Settings, type: :model do

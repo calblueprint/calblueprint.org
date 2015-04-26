13c82dab39869930c24bdd3b45e97bbb5c979f51
Sam Lau <samlau95@gmail.com>
2015-04-26 12:15:04 -0700

    Initalizer to ensure both settings and semester present

---
+++ config/initializers/ensure_semester_and_settings_present.rb
@@ -0,0 +1,33 @@
+# Makes sure at least one semester and one settings model is in the system
+# before the app starts.
+class EnsureSemesterAndSettingsPresent
+  class << self
+    def execute
+      create_first_semester if should_create? Semester
+      create_first_settings if should_create? Settings
+    end
+
+    private
+
+    def create_first_semester
+      today = Date.today
+      year = today.year
+      season = today.month < 6 ? :spring : :fall
+      Semester.create year: year, season: season
+    end
+
+    def create_first_settings
+      Settings.create npo_app_open: true,
+                      student_app_open: true,
+                      current_semester_id: Semester.first.id,
+                      singleton_guard: 0
+    end
+
+    def should_create?(klass)
+      table_name = klass.to_s.tableize
+      ActiveRecord::Base.connection.table_exists?(table_name) && !klass.any?
+    end
+  end
+end
+
+EnsureSemesterAndSettingsPresent.execute
--- config/initializers/semester.rb
+++
@@ -1,7 +0,0 @@
-# Create semester based on current date on initialize
-if ActiveRecord::Base.connection.table_exists?("semesters") && !Semester.any?
-  today = Date.today
-  year = today.year
-  season = today.month < 6 ? :spring : :fall
-  Semester.create year: year, season: season
-end

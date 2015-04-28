b13173b3a7d3b496893a1fd99bc0ad63805449c2
Tricia Fu <triciasfu@gmail.com>
2015-04-27 19:32:57 -0700

    styled current members page

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -1,26 +1,28 @@
-(function (jQuery) {
-  jQuery.mark = {
-    jump: function (options) {
-      var defaults = {
-        selector: 'a.prev'
-      };
-      if (typeof options == 'string') defaults.selector = options;
-      var options = jQuery.extend(defaults, options);
-      return jQuery(options.selector).click(function (e) {
-        var jumpobj = jQuery(this);
-        var target = jumpobj.attr('href');
-        var thespeed = 1000;
-        var offset = jQuery(target).offset().top;
-        jQuery('html,body').animate({
-          scrollTop: offset
-        }, thespeed, 'swing')
-        e.preventDefault();
-      })
+$(document).on('page:change', function() {
+    (function (jQuery) {
+    jQuery.mark = {
+      jump: function (options) {
+        var defaults = {
+          selector: 'a.prev'
+        };
+        if (typeof options == 'string') defaults.selector = options;
+        var options = jQuery.extend(defaults, options);
+        return jQuery(options.selector).click(function (e) {
+          var jumpobj = jQuery(this);
+          var target = jumpobj.attr('href');
+          var thespeed = 1000;
+          var offset = jQuery(target).offset().top;
+          jQuery('html,body').animate({
+            scrollTop: offset
+          }, thespeed, 'swing')
+          e.preventDefault();
+        })
+      }
     }
-  }
-})(jQuery);
+  })(jQuery);
 
+  jQuery(function(){  
+    jQuery.mark.jump();
+  });
 
-jQuery(function(){  
-  jQuery.mark.jump();
 });

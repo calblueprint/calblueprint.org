da11ea23b98ce64e10008c288fab08c94b0059f6
Quinton Dang <qdang16@berkeley.edu>
2015-04-27 19:54:07 -0700

    Fixed mailer error in tests for codeship

--- app/mailers/nonprofit_application_mailer.rb
+++ app/mailers/nonprofit_application_mailer.rb
@@ -1,10 +1,7 @@
 class NonprofitApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-
   def email(nonprofit_application)
     @nonprofit_application = nonprofit_application
     mail to: nonprofit_application.nonprofit.email,
-         from: ENV["TEAM_EMAIL"],
          subject: "Submission of nonprofit application was successful!"
   end
 end

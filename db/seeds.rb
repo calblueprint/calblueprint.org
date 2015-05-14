5927771422d0f0344fc61a85a03eb4c3e4989b50
Sam Lau <samlau95@gmail.com>
2015-05-13 20:28:45 -0700

    Rubocop ignore seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,8 @@
 
 def seed_admins
   Admin.create_with(password: 'password').find_or_create_by!(
-    first_name: 'Blueprint', last_name: 'Admin', email: 'admin@calblueprint.org'
+    first_name: 'Blueprint', last_name: 'Admin',
+                              email: 'admin@calblueprint.org'
   )
 end
 
@@ -65,59 +66,86 @@ def seed_roles_and_members
   corporate_role = MemberRole.find_or_create_by! role: "Corporate Relations Director"
   alumnus_role = MemberRole.find_or_create_by! role: "Alumnus"
 
-  Member.find_or_create_by! first_name: 'Jessica', last_name: 'Lin', email: 'heyjaylin@gmail.com',
+  Member.find_or_create_by! first_name: 'Jessica', last_name: 'Lin',
+                            email: 'heyjaylin@gmail.com',
                             member_role: president_role
-  Member.find_or_create_by! first_name: 'Sam', last_name: 'Lau', email: 'samlau95@gmail.com',
+  Member.find_or_create_by! first_name: 'Sam', last_name: 'Lau',
+                            email: 'samlau95@gmail.com',
                             member_role: tech_role
-  Member.find_or_create_by! first_name: 'Howard', last_name: 'Chen', email: 'howardxchen@gmail.com',
+  Member.find_or_create_by! first_name: 'Howard', last_name: 'Chen',
+                            email: 'howardxchen@gmail.com',
                             member_role: internal_role
-  Member.find_or_create_by! first_name: 'Vivek', last_name: 'Raghuram', email: 'vv9295@yahoo.com',
+  Member.find_or_create_by! first_name: 'Vivek', last_name: 'Raghuram',
+                            email: 'vv9295@yahoo.com',
                             member_role: external_role
-  Member.find_or_create_by! first_name: 'Alice', last_name: 'Oh', email: 'missonederland@gmail.com',
+  Member.find_or_create_by! first_name: 'Alice', last_name: 'Oh',
+                            email: 'missonederland@gmail.com',
                             member_role: alumnus_role
-  Member.find_or_create_by! first_name: 'Eric', last_name: 'Shen', email: 'email.ericshen@gmail.com',
+  Member.find_or_create_by! first_name: 'Eric', last_name: 'Shen',
+                            email: 'email.ericshen@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Nikhil', last_name: 'Narayen', email: 'nikhil.narayen@gmail.com',
+  Member.find_or_create_by! first_name: 'Nikhil', last_name: 'Narayen',
+                            email: 'nikhil.narayen@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Byron', last_name: 'Zhang', email: 'byron.yi.zhang@gmail.com',
+  Member.find_or_create_by! first_name: 'Byron', last_name: 'Zhang',
+                            email: 'byron.yi.zhang@gmail.com',
                             member_role: leader_role
-  Member.find_or_create_by! first_name: 'Harrison', last_name: 'Tsai', email: 'harrisontsai0123@gmail.com',
+  Member.find_or_create_by! first_name: 'Harrison', last_name: 'Tsai',
+                            email: 'harrisontsai0123@gmail.com',
                             member_role: alumnus_role
-  Member.find_or_create_by! first_name: 'Erica', last_name: 'Yin', email: 'eriicaericaerica@gmail.com',
+  Member.find_or_create_by! first_name: 'Erica', last_name: 'Yin',
+                            email: 'eriicaericaerica@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Soo', last_name: 'Song', email: 'soosong@berkeley.edu',
+  Member.find_or_create_by! first_name: 'Soo', last_name: 'Song',
+                            email: 'soosong@berkeley.edu',
                             member_role: media_role
-  Member.find_or_create_by! first_name: 'Quinton', last_name: 'Dang', email: 'quintonddang@gmail.com',
+  Member.find_or_create_by! first_name: 'Quinton', last_name: 'Dang',
+                            email: 'quintonddang@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Max', last_name: 'Wolffe', email: 'max.alan.wolffe@gmail.com',
+  Member.find_or_create_by! first_name: 'Max', last_name: 'Wolffe',
+                            email: 'max.alan.wolffe@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Kevin', last_name: 'Shieh', email: 'kevshieh@gmail.com',
+  Member.find_or_create_by! first_name: 'Kevin', last_name: 'Shieh',
+                            email: 'kevshieh@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Warren', last_name: 'Shen', email: 'warrenzshen@gmail.com',
+  Member.find_or_create_by! first_name: 'Warren', last_name: 'Shen',
+                            email: 'warrenzshen@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Will', last_name: 'Tang', email: 'williamctang93@gmail.com',
+  Member.find_or_create_by! first_name: 'Will', last_name: 'Tang',
+                            email: 'williamctang93@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Aleks', last_name: 'Kamko', email: 'aykamko@gmail.com',
+  Member.find_or_create_by! first_name: 'Aleks', last_name: 'Kamko',
+                            email: 'aykamko@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Varun', last_name: 'Rau', email: 'varun@berkeley.edu',
+  Member.find_or_create_by! first_name: 'Varun', last_name: 'Rau',
+                            email: 'varun@berkeley.edu',
                             member_role: leader_role
-  Member.find_or_create_by! first_name: 'Nishant', last_name: 'Desai', email: 'nish.sdesai@gmail.com',
+  Member.find_or_create_by! first_name: 'Nishant', last_name: 'Desai',
+                            email: 'nish.sdesai@gmail.com',
                             member_role: leader_role
-  Member.find_or_create_by! first_name: 'Jordeen', last_name: 'Chang', email: 'jordeenchang@gmail.com',
+  Member.find_or_create_by! first_name: 'Jordeen', last_name: 'Chang',
+                            email: 'jordeenchang@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Sameera', last_name: 'Vemulapalli', email: 'sameera.vemulapalli@gmail.com',
+  Member.find_or_create_by! first_name: 'Sameera', last_name: 'Vemulapalli',
+                            email: 'sameera.vemulapalli@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Ethan', last_name: 'Chiou', email: 'nahte.uoihc@gmail.com',
+  Member.find_or_create_by! first_name: 'Ethan', last_name: 'Chiou',
+                            email: 'nahte.uoihc@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Atsunori', last_name: 'Kakitani', email: 'atsunori.kakitani@gmail.com',
+  Member.find_or_create_by! first_name: 'Atsunori', last_name: 'Kakitani',
+                            email: 'atsunori.kakitani@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Elizabeth', last_name: 'Lin', email: 'elizabethylin@gmail.com',
+  Member.find_or_create_by! first_name: 'Elizabeth', last_name: 'Lin',
+                            email: 'elizabethylin@gmail.com',
                             member_role: alumnus_role
-  Member.find_or_create_by! first_name: 'Andy', last_name: 'Qin', email: 'andrewlqin@gmail.com',
+  Member.find_or_create_by! first_name: 'Andy', last_name: 'Qin',
+                            email: 'andrewlqin@gmail.com',
                             member_role: developer_role
-  Member.find_or_create_by! first_name: 'Jason', last_name: 'Won', email: 'jason.j.won@gmail.com',
+  Member.find_or_create_by! first_name: 'Jason', last_name: 'Won',
+                            email: 'jason.j.won@gmail.com',
                             member_role: leader_role
-  Member.find_or_create_by! first_name: 'Daniel', last_name: 'Li', email: 'danielmxli@gmail.com',
+  Member.find_or_create_by! first_name: 'Daniel', last_name: 'Li',
+                            email: 'danielmxli@gmail.com',
                             member_role: developer_role
 end
 

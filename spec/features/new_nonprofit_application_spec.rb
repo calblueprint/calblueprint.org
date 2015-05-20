3a00c0be5abe7106dab69f4e8566af4397d0e6b9
Sam Lau <samlau95@gmail.com>
2015-05-20 16:57:53 -0700

    Fix spec for npo applying

--- spec/features/new_nonprofit_application_spec.rb
+++ spec/features/new_nonprofit_application_spec.rb
@@ -1,6 +1,19 @@
 require "rails_helper"
 
 RSpec.describe "Nonprofit Application Form" do
+  # Helpers for this spec
+  def fill_in_form
+    app = build :nonprofit_application
+    [:purpose, :history, :short_summary, :goals, :key_features,
+     :target_audience, :why].each do |attribute|
+       fill_in "nonprofit_application_#{attribute}", with: app.send(attribute)
+    end
+  end
+
+  def submit_form
+    click_button "Submit"
+  end
+
   let(:nonprofit) { create :nonprofit }
 
   before do
@@ -13,37 +26,34 @@ RSpec.describe "Nonprofit Application Form" do
 
   subject { page }
 
-  it { should have_content "looking forward" }
+  it { should have_content t("nonprofit_applications.new.subtitle") }
 
   describe "without filling in form" do
     it "renders back form with errors" do
-      click_button "Submit"
-      expect(page).to have_content "looking forward"
+      submit_form
+      expect(page).to have_content t("nonprofit_applications.new.subtitle")
       expect(page).to have_content "can't be blank"
     end
 
     it "does not send email" do
-      expect { click_button "Submit" }.not_to change { ActionMailer::Base.deliveries.count }
+      expect { submit_form }.not_to change { ActionMailer::Base.deliveries.count }
     end
   end
 
-  pending "after filling in form" do
-    before do
-      fill_in "Purpose", with: "To change the world."
-    end
+  describe "after filling in form" do
+    before { fill_in_form }
 
     it "redirects and renders a success message on submit" do
-      click_button "Submit"
-      expect(page).not_to have_content t("nonprofit_applications.new.banner")
+      submit_form
       expect(page).to have_content t("nonprofit_applications.create.success")
     end
 
     it "creates a Nonprofit Application on submit" do
-      expect { click_button "Submit" }.to change { NonprofitApplication.count }.by(1)
+      expect { submit_form }.to change { NonprofitApplication.count }.by(1)
     end
 
     it "sends an email on submit" do
-      expect { click_button "Submit" }.to change { ActionMailer::Base.deliveries.count }.by(1)
+      expect { submit_form }.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
   end
 end

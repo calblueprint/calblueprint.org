06786ec7983a263c30d33114fadb4d1e1089fa24
Sam Lau <samlau95@gmail.com>
2015-05-19 19:35:50 -0700

    NPO show page! Look at that code tho

--- app/controllers/admins/nonprofit_applications_controller.rb
+++ app/controllers/admins/nonprofit_applications_controller.rb
@@ -1,5 +1,10 @@
 module Admins
   class NonprofitApplicationsController < BaseController
+    def show
+      application = NonprofitApplication.find(params[:id])
+      @nonprofit_application = NonprofitApplicationDecorator.new application
+    end
+
     def index
       @nonprofit_applications = NonprofitApplication.all
     end

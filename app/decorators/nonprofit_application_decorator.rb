5e0ddae3e19269890984e1eeeb48013ebe578923
Sam Lau <samlau95@gmail.com>
2015-05-19 19:39:05 -0700

    Clean up some more

--- app/decorators/nonprofit_application_decorator.rb
+++ app/decorators/nonprofit_application_decorator.rb
@@ -8,17 +8,7 @@ class NonprofitApplicationDecorator < ApplicationDecorator
 
   def attribute_html(attribute)
     h.content_tag :div, class: "two-col-container" do
-      left_col = h.content_tag :div, class: "two-col-left" do
-        h.content_tag :h4 do
-          object.class.human_attribute_name(attribute)
-        end
-      end
-
-      right_col = h.content_tag :div, class: "two-col-right" do
-        h.simple_format send(attribute)
-      end
-
-      left_col + right_col
+      left_col_html(attribute) + right_col_html(attribute)
     end
   end
 
@@ -32,5 +22,21 @@ class NonprofitApplicationDecorator < ApplicationDecorator
 
   def devices
     object.devices.reject(&:empty?).join(", ")
+  end
+
+  private
+
+  def left_col_html(attribute)
+    h.content_tag :div, class: "two-col-left" do
+      h.content_tag :h4 do
+        object.class.human_attribute_name(attribute)
+      end
+    end
+  end
+
+  def right_col_html(attribute)
+    h.content_tag :div, class: "two-col-right" do
+      h.simple_format send(attribute)
+    end
   end
 end

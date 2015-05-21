64d575002e0c10f562619ef6c44cfe41b4fddcfc
Sam Lau <samlau95@gmail.com>
2015-05-20 17:37:47 -0700

    Admin NPO app spec

---
+++ spec/features/admin/nonprofit_applications_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "Nonprofit applications" do
+  let(:admin) { create :admin }
+  let!(:app1) { create :nonprofit_application }
+  let!(:app2) { create :nonprofit_application }
+
+  subject { page }
+
+  before { sign_in admin }
+
+  describe "index page" do
+    before { visit admin_nonprofit_applications_path }
+
+    it { should have_content t("admins.nonprofit_applications.index.banner") }
+
+    it "shows each nonprofit app" do
+      [app1, app2].each do |app|
+        within("tr##{dom_id app}") do
+          expect(page).to have_content app.email
+        end
+      end
+    end
+  end
+
+  describe "show page" do
+    before { visit admin_nonprofit_application_path app1 }
+
+    it { should have_content app1.organization_name }
+    it { should have_content app1.why }
+  end
+end

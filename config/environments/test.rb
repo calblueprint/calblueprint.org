8b6ec154cace23ed37a248fb73b88160add7ea45
Howard Chen <howardxchen@gmail.com>
2015-05-22 18:38:54 -0700

    Fix hound complaints

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_files  = true
+  config.serve_static_files = true
   config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
@@ -41,5 +41,4 @@ Rails.application.configure do
 
   # Propagate callback errors
   config.active_record.raise_in_transactional_callbacks = true
-
 end

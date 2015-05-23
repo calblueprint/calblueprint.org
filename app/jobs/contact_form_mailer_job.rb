1148e6e740f5a6224d401ac271fce4c78901a7db
Howard Chen <howardxchen@gmail.com>
2015-05-22 17:53:07 -0700

    Change deliver to deliver_now due to deprecation

--- app/jobs/contact_form_mailer_job.rb
+++ app/jobs/contact_form_mailer_job.rb
@@ -2,6 +2,6 @@ class ContactFormMailerJob
   include SuckerPunch::Job
 
   def perform(contact_form)
-    ContactFormMailer.email(contact_form).deliver
+    ContactFormMailer.email(contact_form).deliver_now
   end
 end

ffcc043d657d6bf9c15ca9b0858059f6e27a53ac
Howard Chen <howardxchen@gmail.com>
2015-08-25 13:43:11 -0700

    \More hound errors

--- app/models/student_application.rb
+++ app/models/student_application.rb
@@ -38,6 +38,6 @@ class StudentApplication < ActiveRecord::Base
   validates :name, presence: true
 
   validates :email,
-    format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
-    presence: true
+            format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+            presence: true
 end

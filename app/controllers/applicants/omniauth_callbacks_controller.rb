de1f29d4ef239e31225b8b310cdac5cf7bf15ca9
Sam Lau <samlau95@gmail.com>
2015-08-25 02:03:02 -0700

    Slightly better logged in flash message Before, the flash message would say something like "Successfully logged in from Google_oauth2 account.". Now, it just says "Google account".

--- app/controllers/applicants/omniauth_callbacks_controller.rb
+++ app/controllers/applicants/omniauth_callbacks_controller.rb
@@ -8,7 +8,8 @@ module Applicants
 
           if @applicant.persisted?
             sign_in_and_redirect @applicant, event: :authentication
-            set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+            set_flash_message(:notice, :success,
+              kind: "#{provider}".titleize.split(' ').first) if is_navigational_format?
           else
             session["devise.#{provider}_data"] = env["omniauth.auth"]
             redirect_to new_user_registration_url

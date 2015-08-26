913766e6ee48ab0f5c1168db73e88e395eb75deb
vivekraghuram <VV9295@YAHOO.COM>
2015-08-25 23:21:10 -0700

    Appease hound

--- app/controllers/newsletter_controller.rb
+++ app/controllers/newsletter_controller.rb
@@ -1,14 +1,13 @@
 class NewsletterController < ApplicationController
-
   def subscribe
     email = params[:email]
     list = params[:list]
     begin
       gibbon = Gibbon::Request.new(api_key: ENV["MAILCHIMP_API"])
-      gibbon.lists(ENV[list]).members.create(body: {email_address: email, status: "subscribed"})
-      render :json => { }
-    rescue =>e
-      render :json => { }, :status => 500
+      gibbon.lists(ENV[list]).members.create(body: { email_address: email, status: "subscribed" })
+      render json: {}
+    rescue
+      render json: {}, status: 500
     end
   end
 end

34b7c6571b870e4087c91cf6b9c4b9de52e438c9
Aleks Kamko <aykamko@gmail.com>
2015-09-01 11:01:49 -0700

    fixed resume link

--- app/decorators/student_application_decorator.rb
+++ app/decorators/student_application_decorator.rb
@@ -4,14 +4,16 @@ class StudentApplicationDecorator < ApplicationDecorator
   def display_attributes
     [:year,
      :why_join,
-     [:resume_file_name, "Resume"],
+     :resume,
      [:available_for_bp_games, "Available for BP Games?"],
      [:available_for_retreat, "Available for Retreat?"],
      :phone,
      :email]
   end
 
-  def resume_file_name
-    h.link_to "Link", object.resume_file_name
+  def resume
+    h.link_to "Link", object.resume.url
   end
+
+
 end

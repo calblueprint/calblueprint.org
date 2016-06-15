7bc28fc7cd3620be7f1d7d885cde164ff005160f
vivekraghuram <VV9295@YAHOO.COM>
2016-06-14 22:14:28 -0700

    Remove console logs

--- app/assets/javascripts/pages/nonprofit_application/save_application.js
+++ app/assets/javascripts/pages/nonprofit_application/save_application.js
@@ -1,5 +1,4 @@
 $(document).on( "page:load ready", function() {
-  console.log("ready");
   $("#save_button").on("click", function(e) {
     var endpoint = $(this).data("endpoint");
     var request = {
@@ -8,16 +7,13 @@ $(document).on( "page:load ready", function() {
       data: $('[id^="edit_nonprofit_application_"').serialize(),
       dataType: "json"
     };
-    console.log(request);
     $.ajax(request).done(function(response) {
       if (response.success) {
         $("#last_updated").text(" " + (new Date().toLocaleString()));
       } else {
-        console.log(response);
         window.alert("Uh oh. Something went wrong.");
       }
     }).fail(function(error) {
-      console.log(error);
       window.alert("Uh oh. Something went wrong.");
     });
   });

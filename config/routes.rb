616adc100e35789ec9b952b8207112461b573c56
vivekraghuram <VV9295@YAHOO.COM>
2016-06-28 23:23:38 -0700

    Delete old models, views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -47,14 +47,7 @@ Rails.application.routes.draw do
       collection { post :import }
     end
 
-    resources :members
-    resources :member_roles, only: [:index, :new, :create, :destroy]
     resources :semesters
-
-    resources :projects, except: [:show] do
-      member { post :toggle_publish }
-      collection { post :change_position }
-    end
 
     resources :final_decisions, only: [] do
       member do

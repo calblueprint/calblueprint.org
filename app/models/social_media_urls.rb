be4c3ea5d53cbc8cc96c5f6fd36e029f989932ce
Eric Liang <liang.eric24@gmail.com>
2017-05-22 16:05:58 -0700

    small fixes

--- app/models/social_media_urls.rb
+++ app/models/social_media_urls.rb
@@ -1,4 +1,7 @@
 class SocialMediaUrls
+  # Post links to infosession Facebook events here
+  INFOSESSION_1 = nil
+  INFOSESSION_2 = nil
   FACEBOOK = "http://www.facebook.com/calblueprint"
   TWITTER = "https://twitter.com/calblueprint"
   GITHUB = "https://github.com/calblueprint/"

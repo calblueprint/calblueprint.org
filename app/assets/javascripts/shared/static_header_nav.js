d7b48c0527b83b3911410ef5b79d386260260ede
Eric Liang <liang.eric24@gmail.com>
2017-05-22 13:10:40 -0700

    fixed small rendering errors

--- app/assets/javascripts/shared/static_header_nav.js
+++ app/assets/javascripts/shared/static_header_nav.js
@@ -1,9 +1,9 @@
-$(window).on("ready page:load", function() {
+$(document).on("ready page:load", function() {
   $(".static-page-hero .page-nav .page-nav-link").each(function(index, el) {
     $(el).click(function() {
       var sectionName = $(this).attr("section-name");
       $('html, body').animate({
-          scrollTop: $("#" + sectionName).offset().top
+        scrollTop: $("#" + sectionName).offset().top
       }, 400);
     })
   })

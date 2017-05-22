4cf72806a99b46392e8680ffbad5b0f5ac391a29
Eric Liang <liang.eric24@gmail.com>
2017-05-22 16:26:22 -0700

    fix js parse error

--- app/assets/javascripts/pages/_navbar.js
+++ app/assets/javascripts/pages/_navbar.js
@@ -1,5 +1,5 @@
-$(window).scroll((e) => {
-  if ($(this).scrollTop() > 400) {
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= 400) {
     $(".bp-nav-fixed").addClass("nav-fixed-show");
   } else {
     $(".bp-nav-fixed").removeClass("nav-fixed-show");

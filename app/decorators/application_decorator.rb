4d370c09c653685543a3a874b168c1624ebc2fb8
Eric Liang <liang.eric24@gmail.com>
2017-07-06 22:04:22 -0700

    admin panel redesign start

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -3,33 +3,35 @@ class ApplicationDecorator < Draper::Decorator
     pred ? "Yes" : "No"
   end
 
+  def html(attribute)
+    h.content_tag :div, class: "app-item-row" do
+
+    end
+  end
+
   def attribute_html(attribute)
-    h.content_tag :div, class: "two-col-container" do
+    h.content_tag :div, class: "app-row" do
       left_col_html(attribute) + right_col_html(attribute)
     end
   end
 
   def left_col_html(attribute)
-    h.content_tag :div, class: "two-col-left" do
-      h.content_tag :h4 do
-        if attribute.is_a?(Array)
-          attribute[1]
-        else
-          object.class.human_attribute_name(attribute)
-        end
+    h.content_tag :h3, class: "heading" do
+      if attribute.is_a?(Array)
+        attribute[1]
+      else
+        object.class.human_attribute_name(attribute)
       end
     end
   end
 
   def right_col_html(attribute)
-    h.content_tag :div, class: "two-col-right" do
-      val = send(attribute.is_a?(Array) ? attribute[0] : attribute)
-      if !!val == val
-        val = yesno(val)
-      elsif val.is_a? Numeric
-        val = val.to_s
-      end
-      h.simple_format val
+    val = send(attribute.is_a?(Array) ? attribute[0] : attribute)
+    if !!val == val
+      val = yesno(val)
+    elsif val.is_a? Numeric
+      val = val.to_s
     end
+    h.simple_format val
   end
 end

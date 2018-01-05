a02c768c51dd9b83a7af5074e3b050925d9b9407
Ken Chen <lbkchen@gmail.com>
2018-01-06 00:33:11 +0800

    More fixes to student application and logger.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,8 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get((ENV["LOG_LEVEL"] || "INFO").upcase)
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]

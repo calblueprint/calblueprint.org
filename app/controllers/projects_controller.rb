4a54d8ee9783942a2d61fcdd3ae326a7efbc7a87
Ken Chen <lbkchen@gmail.com>
2018-03-11 00:22:51 -0800

    Adds some more styles and html for banner.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,18 @@
 class ProjectsController < ApplicationController
   def show
-    @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    # @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    unless params[:current]
+      @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+      render :show
+    end
+
+    slug_to_view = {
+      "DREAM" => :dream
+    }
+    unless slug_to_view.has_key? params[:slug]
+      render '/public/404'
+    end
+    render slug_to_view[params[:slug]]
   end
 
 end

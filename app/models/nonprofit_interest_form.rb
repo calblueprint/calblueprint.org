392247b9cc6c421d1c5cdc5cd83e14a47c33a001
Kevin Li <kevintli98@gmail.com>
2019-06-08 02:08:23 -0700

    NPO interest form: make website link required

--- app/models/nonprofit_interest_form.rb
+++ app/models/nonprofit_interest_form.rb
@@ -14,6 +14,7 @@ class NonprofitInterestForm < ActiveRecord::Base
   validates :role, presence: true
   validates :agree_to_terms, presence: true
   validates :office, presence: true
+  validates :website, presence: true
 
   CATEGORIES = [
     "Web application",

3880733039fb37b2b6db32e2463d77812f0f5f3f
aivantg <aivant.developer@gmail.com>
2020-06-04 22:31:04 -0500

    small subject line update

--- app/mailers/nonprofit_interest_form_mailer.rb
+++ app/mailers/nonprofit_interest_form_mailer.rb
@@ -2,6 +2,6 @@ class NonprofitInterestFormMailer < ActionMailer::Base
   def email(nonprofit_interest_form)
     @nonprofit_interest_form = nonprofit_interest_form
     mail to: nonprofit_interest_form.nonprofit.email,
-         subject: "Thanks for submitting the interest form!"
+         subject: "Thanks for submitting the interest form! Looking forward to our call"
   end
 end

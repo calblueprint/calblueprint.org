2ecb5b70b709c096690559975663e3d6eca1f75a
Frederick (Kyu Won) Kim <freddy.kim8@gmail.com>
2020-07-27 20:47:48 -0700

    Warn that application save doesn't submit (#388)

--- app/assets/javascripts/pages/nonprofit_application/save_application.js
+++ app/assets/javascripts/pages/nonprofit_application/save_application.js
@@ -1,23 +1,27 @@
-$(document).on( "page:load ready", function() {
-  $("#save_button").on("click", function(e) {
+$(document).on("page:load ready", function () {
+  $("#save_button").on("click", function (e) {
     e.preventDefault();
     var endpoint = $(this).data("endpoint");
     var request = {
       method: "POST",
       url: endpoint,
       data: $('[id^="edit_nonprofit_application_"').serialize(),
-      dataType: "json"
+      dataType: "json",
     };
-    $.ajax(request).done(function(response) {
-      if (response.success) {
-        $("#last_updated").text(" " + (new Date().toLocaleString()));
-        toastr.success("Application saved!");
-      } else {
+    $.ajax(request)
+      .done(function (response) {
+        if (response.success) {
+          $("#last_updated").text(" " + new Date().toLocaleString());
+          toastr.success(
+            "Application draft saved! Click the submit button if you wish to submit."
+          );
+        } else {
+          window.alert("Uh oh. Something went wrong.");
+        }
+      })
+      .fail(function (error) {
         window.alert("Uh oh. Something went wrong.");
-      }
-    }).fail(function(error) {
-      window.alert("Uh oh. Something went wrong.");
-    });
+      });
 
     return false;
   });

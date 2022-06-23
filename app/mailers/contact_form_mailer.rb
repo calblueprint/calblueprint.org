70d1227b6b92471ad19d2703552ba16870a7a672
Jacob Kim <jacobjk01@berkeley.edu>
2022-06-22 21:23:48 -0700

    Jacob/fall2022 (#465)
    * fixing recruitment dates and partially editing member roles
    
    * fa2022 npo application updates

--- app/mailers/contact_form_mailer.rb
+++ app/mailers/contact_form_mailer.rb
@@ -1,7 +1,8 @@
 class ContactFormMailer < ActionMailer::Base
   def email(contact_form)
     @contact_form = contact_form
-    mail to: ENV["TEAM_EMAIL"],
+    # mail to: ENV["TEAM_EMAIL"],
+    mail to: "projects@calblueprint.org"
          from: contact_form.email,
          subject: contact_form.subject
   end

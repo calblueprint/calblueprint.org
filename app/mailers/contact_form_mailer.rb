bc851cdc3bce67ee1a960dd49e5265d572835e2d
Jacob Kim <jacobjk01@berkeley.edu>
2022-06-22 21:33:17 -0700

    syntax issue

--- app/mailers/contact_form_mailer.rb
+++ app/mailers/contact_form_mailer.rb
@@ -2,7 +2,7 @@ class ContactFormMailer < ActionMailer::Base
   def email(contact_form)
     @contact_form = contact_form
     # mail to: ENV["TEAM_EMAIL"],
-    mail to: "projects@calblueprint.org"
+    mail to: "projects@calblueprint.org",
          from: contact_form.email,
          subject: contact_form.subject
   end

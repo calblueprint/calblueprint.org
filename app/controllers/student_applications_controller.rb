4e6e5b9611aaf192adaf4c8e3a84a232c507971b
Noah Hernandez <63211322+oahnh@users.noreply.github.com>
2024-01-25 14:56:04 -0800

    remove redirect to demographic survey

--- app/controllers/student_applications_controller.rb
+++ app/controllers/student_applications_controller.rb
@@ -43,7 +43,9 @@ class StudentApplicationsController < ApplicationController
       if @student_application.save
         applicant.update_attributes(name: @student_application.name)
         SendStudentApplicationEmail.execute @student_application
-        redirect_to new_demographic_survey_path, flash: { success: t('student_applications.create.success') }
+        #sp24: added demographic questions -> main
+        # redirect_to new_demographic_survey_path, flash: { success: t('student_applications.create.success') }
+        redirect_to root_path, flash: { success: t('student_applications.create.success') }
       else
         render "new"
       end
